6b5f8eb25b9d862a246b8e0e0cab296d498bab53
Gabriel <gabrielldarts@gmail.com>
2020-10-09 20:48:46 -0300

    complete

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,11 +1,32 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
 class Category {
+  find(arg0: { where: { title: import("typeorm").FindOperator<any>; }; }) {
+    throw new Error('Method not implemented.');
+  }
+  save(newCategories: any) {
+    throw new Error('Method not implemented.');
+  }
+  
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
+  create: any;
 }
 
 export default Category;
